e6053182646b78f6de078af9fd7baf44fc439e52
Нурбол Сембаев <231199@astanait.edu.kz>
2024-02-28 09:01:04 +0600

    nurok

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,61 +1,16 @@
-package models;
+package com.example.springprojectdemo.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.Data;
 
-public class Person{ ;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-@Getter
-@Setter
+@Data
+@Entity
+@Table(name = "users")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private int age;
-    private String email;
-
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
-{
+    private String surname;
+    private boolean gender;
 }

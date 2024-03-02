4c0c0cfea6db6ee8817f7c78b7c92052fc8e2278
aaaether <153725425+aaaether@users.noreply.github.com>
2024-03-02 12:03:10 +0600

    Create Person.java

---
+++ src/main/java/com/example/springprojectdemo/models/Person.java
@@ -0,0 +1,22 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+}

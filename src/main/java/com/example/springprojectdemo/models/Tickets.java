2d609e420b17e2162e8742152a2697e93caf5a8c
aaaether <153725425+aaaether@users.noreply.github.com>
2024-03-02 12:52:50 +0600

    Update Tickets.java

--- src/main/java/com/example/springprojectdemo/models/Tickets.java
+++ src/main/java/com/example/springprojectdemo/models/Tickets.java
@@ -1,14 +1,16 @@
 package com.example.springprojectdemo.models;
 
-import jakarta presistence.*;
+import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDate;
-
 
 public class Tickets {
     private int id;
     private String time;
     private String date;
+    private double price;
+    private int personId;
+    private String personName;
 
     public Tickets(int id, String time, String date) {
         this.id = id;
@@ -39,4 +41,29 @@ public class Tickets {
     public void setDate(String date) {
         this.date = date;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
 }
+    
